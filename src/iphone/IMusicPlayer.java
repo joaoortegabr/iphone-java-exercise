d124fc612165578438a92fba2a4ce96d0dfe5bee
Joao Ortega <joaoortegabr@gmail.com>
2022-07-15 03:10:55 -0300

    Add files via upload

--- src/iphone/IMusicPlayer.java
+++ src/iphone/IMusicPlayer.java
@@ -1,12 +1,6 @@
 package iphone;
 
 interface IMusicPlayer {
-
-	// all variables are public static final
-		// static can't be instantiate
-		// final can't be changed
-	// all methods are public abstract
-		// abstract can't be implemented - only formats it
 	
 	void play(String songName);
 	
